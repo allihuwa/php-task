87b69ddfffd0f1d710543f0ef333aa74d3426e0b
Yuri Cabrita <yuricabrita@Yuris-MacBook-Pro.local>
2023-09-21 18:35:55 +1000

    add sanitization for csv data, check for duplicates and wrong emails, add a dry run option where we only sanitize data and dont add to the database, fortify the while(true) logic

--- user_upload.php
+++ user_upload.php
@@ -1,15 +1,20 @@
 <?php
 // Define default MySQL connection details
 $host = 'localhost';
-$username = 'default_username';
-$password = 'default_password';
-$database = 'default_database';
+$username = 'root';
+$database = 'test';
+$password = '';
 
 while (true) {
 	echo "Enter a command (type '--help' for help, or 'exit' to quit): ";
 	$stdin = trim(fgets(STDIN));
 	$command = explode(' ', $stdin);
 
+	// Count number of arguments
+	if (count($command) > 2) {
+		echo "Too many arguments.\n";
+	}
+	
 	// Exit command
 	if ($command[0] === 'exit') {
 		echo "Goodbye!\n";
@@ -66,14 +71,101 @@ while (true) {
 
 		echo "MySQL users table created successfully.\n";
 		$mysqli->close();
-		exit;
 	}
 
 	// Check that a valid file exists and that the user provides a file name or print out an error message
-	elseif ($command[0] === '--file') {
+	elseif ($command[0] === '--file' || $command[0] === '--dry-run') {
+
+		//Check if file exists
 		if(!file_exists($command[1])) {
 			echo("Error: Please provide a valid CSV file using the --file option.\n");
 		}
+
+		// Open and read the CSV file
+		$csvFile = fopen($command[1], 'r');
+		if ($csvFile === false) {
+				echo("Error: Unable to open CSV file.\n");
+				break;
+		}
+
+		//Sanitize the CSV data for duplicates and incorrect emails
+		$uniqueData = sanitizeData($csvFile);
+
+		//Add to database table if not a dry-run
+		if($command[0] === '--file') {
+			addToDatabase($uniqueData, $host, $username, $password, $database);
+		}
+
+		if($command[0] === '--dry-run') {
+			
+			echo "Printing out sanitzied data for dry run:\n";
+			// Iterate through the sanitized data and print each record
+			foreach ($uniqueData as $record) {
+				$name = $record[0];
+				$surname = $record[1];
+				$email = $record[2];
+				
+				echo "Name: $name, Surname: $surname, Email: $email\n";
+			}
+			echo "Dry run complete.\n";
+		}
+
+		// Close resources
+		fclose($csvFile);
 	}
+}
+
+function sanitizeData($filename) {
+	$sanitized_data = [];
+	// Iterate through CSV rows
+	while (($data = fgetcsv($filename)) !== false) {
+		$name = ucfirst(strtolower(trim($data[0])));
+		$surname = ucfirst(strtolower(trim($data[1])));
+		$email = strtolower(trim($data[2]));
+
+		// Validate email format
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				echo "Invalid email format: $email - skipping to next record\n";
+				continue; // Skip this record and move to the next
+		}
+
+		// Check if the email already exists in $uniqueData
+		$existingKeys = array_keys(array_column($sanitized_data, 2), $email);
+
+		if (empty($existingKeys)) {
+				// Email doesn't exist in $uniqueData, add the record
+				$sanitized_data[] = [$name, $surname, $email];
+		} else {
+				// Email already exists, report a duplicate
+				echo "Duplicate email found: $email\n for user with name $name and surname $surname, skipping record\n";
+		}
+	}
+
+	echo ("Data succesfully sanitized.\n");
+	return $sanitized_data;
+}
+
+function addToDatabase($data, $host, $username, $password, $database) {
+	$mysqli = new mysqli($host, $username, $password, $database);
+	
+	// Iterate through the unique data and insert into MySQL table
+	foreach ($data as $record) {
+		$name = $record[0];
+		$surname = $record[1];
+		$email = $record[2];
+
+		// Insert data into MySQL table
+		$insertQuery = "INSERT INTO users (name, surname, email) VALUES (?, ?, ?)";
+		$stmt = $mysqli->prepare($insertQuery);
+		$stmt->bind_param("sss", $name, $surname, $email);
+
+		if (!$stmt->execute()) {
+				echo "Error inserting data: " . $stmt->error . "\n";
+		} else {
+				echo "Record inserted successfully: $name, $surname, $email\n";
+		}
+	}
+
+	$mysqli->close();
 }
 ?>
