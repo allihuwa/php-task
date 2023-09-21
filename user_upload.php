50b8cb2841f130b6e761b6f3beee9b8ba81cc5f4
Yuri Cabrita <yuricabrita@Yuris-MacBook-Pro.local>
2023-09-21 16:54:21 +1000

    Define mySQL connection variables, command line options and related help message

---
+++ user_upload.php
@@ -0,0 +1,25 @@
+<?php
+// Define MySQL connection details
+$host = 'localhost';
+$username = '';
+$password = '';
+$db = '';
+
+// Define command line options
+$options = getopt("u:p:h:", ["file:", "create_table", "dry_run", "help"]);
+
+// Display help message
+if (isset($options['help'])) {
+    echo "PHP User Upload Script\n";
+    echo "Usage:\n";
+    echo "  --file [csv file name] - Name of the CSV to be parsed\n";
+    echo "  --create_table - Create the MySQL users table and exit\n";
+    echo "  --dry_run - Parse the CSV but do not insert into the DB\n";
+    echo "  -u - MySQL username\n";
+    echo "  -p - MySQL password\n";
+    echo "  -h - MySQL host\n";
+    echo "  --help - Display this help message\n";
+    exit;
+}
+
+?>
